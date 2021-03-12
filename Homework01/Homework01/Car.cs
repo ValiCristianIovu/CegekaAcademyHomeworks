8a9000154723c8dd428ba7b7bed07b20689327f8
ATLASMLS <45317329+ATLASMLS@users.noreply.github.com>
2021-03-12 14:13:05 +0200

    Add Homework01 for CegekaAcademy2021
    Implement Car class.
    Implement Console Menu.

---
+++ Homework01/Homework01/Car.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework01
+{
+    public enum Motorization
+    {
+        Gas,
+        Diesel,
+        Electric,
+        Hybrid,
+        Unknown
+    }
+    public enum Manufacter
+    {
+        Volvo,
+        Kia,
+        Hyundai,
+        Toyota,
+        Subaru,
+        Ford,
+        Dacia,
+        UTB,
+        Unknown
+    }
+    public enum Model
+    {
+        Micro,
+        Sedan,
+        CUV,
+        SUV,
+        Hatchback,
+        Roadster,
+        Pickup,
+        Van,
+        Coupe,
+        Supercar,
+        Minivan,
+        Truck,
+        U650,
+        Unknown
+    }
+    public enum Packages
+    {
+        Entry,
+        Plus,
+        Premium,
+        Sport,
+        Farmer,
+        Unknown
+    }
+    public class Car
+    {
+        public static int counter;
+        public Manufacter manufacter;
+        public Model model;
+        public String color;
+        public Motorization motorization;
+        public Packages package;
+
+        public Car()
+        {
+            this.manufacter = Manufacter.Unknown;
+            this.model = Model.Unknown;
+            this.color = "Unknown";
+            this.motorization = Motorization.Unknown;
+            this.package = Packages.Unknown;
+            counter++;
+        }
+        public Car(Manufacter manufacter, Model model, String colorName, Motorization motorizationType ,Packages package)
+        {
+            this.manufacter = manufacter;
+            this.model = model;
+            this.color = colorName;
+            this.motorization = motorizationType;
+            this.package = package;
+            counter++;
+
+        }
+        public static int GetNumOfInstances()
+        {
+            return counter;
+        }
+        public void CarModelsPrint()
+        {
+            
+                Console.WriteLine(this.color + " " + this.manufacter +" "+ this.model + " with " + this.motorization + " motor " + "equiped with the " + this.package + " package");
+            
+        }
+
+        public Manufacter GetManufacter()
+        {
+           return manufacter; 
+           
+        }
+        public void SetManufacter(int i)
+        {
+            this.manufacter = (Manufacter)i;
+        }
+
+        public String GetColor()
+        {
+            return color;
+        }
+       
+        public void SetColor(string colorr)
+        {
+            this.color = colorr;
+        }
+
+        public Model GetModel()
+        {
+            return model;
+        }
+
+        public void SetModel(int i)
+        {
+            this.model = (Model)i;
+        }
+        public Motorization GetMotorization()
+        {
+            return motorization;
+        }
+        public void SetMotorization(int i)
+        {
+            this.motorization = (Motorization)i;
+        }
+        public Packages GetPackages()
+        {
+            return package;
+        }
+        public void SetPackage(int i)
+        {
+            this.package = (Packages)i;
+        }
+    }
+}
---
+++ Homework01/Homework01/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework01
+{
+   
+    class Program
+    {
+        private static List<Car> myCars = new List<Car>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t Hidden Leaf Village  Dealership:");
+
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+           
+        }
+
+        private static bool MainMenu()
+        {
+            //Console.Clear();
+            Console.WriteLine("\nChoose an option:");
+            Console.WriteLine("1) Create a new car");
+            Console.WriteLine("2) Print manufactered cars");
+            Console.WriteLine("3) See the number of manufactered cars");
+            Console.WriteLine("4) Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    PrintOptions();
+                    CreateCar();
+                    return true;
+                case "2":
+                    PrintManufacteredCars();
+                    return true;
+                case "3":
+                    PrintNrOfManufacteredCars();
+                    return false;
+                case "4":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        private static void PrintNrOfManufacteredCars()
+        {
+            Console.WriteLine("The amount of cars manufactered is equal to :" + Car.GetNumOfInstances()+"\n");
+        }
+
+        private static void PrintManufacteredCars()
+        {
+            Console.WriteLine("The manufactered cars are : ");
+            foreach (Car c in myCars)
+            {
+                c.CarModelsPrint();
+            }
+        }
+
+        private static void PrintOptions()
+        {
+            Console.WriteLine("For creating a car we have the next possibilities: ");
+            Console.WriteLine("Types of Manufacteres: ");
+            foreach (Manufacter val in Enum.GetValues(typeof(Manufacter)))
+            {
+                Console.Write(val+" ");
+            }
+            Console.WriteLine("\nTypes of Models: ");
+            foreach (Model val in Enum.GetValues(typeof(Model)))
+            {
+                Console.Write(val + " ");
+            }
+            Console.WriteLine("\nTypes of Packages: ");
+            foreach (Packages val in Enum.GetValues(typeof(Packages)))
+            {
+                Console.Write(val + " ");
+            }
+            Console.WriteLine("\n");
+
+
+        }
+
+        private static void CreateCar()
+        {
+            
+            Car carObject = new Car();
+            Console.WriteLine("Car Color: ");
+            var x = Console.ReadLine();
+            carObject.SetColor(x);
+            Console.WriteLine("Car Manufacter: ");
+            x = Console.ReadLine();
+            carObject.SetManufacter(int.Parse(x));
+            Console.WriteLine("Car Model: ");
+            x = Console.ReadLine();
+            carObject.SetModel(int.Parse(x));
+            Console.WriteLine("Car Motorization: ");
+            x = Console.ReadLine();
+            carObject.SetMotorization(int.Parse(x));
+            Console.WriteLine("Car Package: ");
+            x = Console.ReadLine();
+            carObject.SetPackage(int.Parse(x));
+
+            carObject.CarModelsPrint();
+            myCars.Add(carObject);
+            Console.WriteLine("\n\n");
+            
+        }
+    }
+}
